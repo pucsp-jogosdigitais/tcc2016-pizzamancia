b4de3c849942cbc21c7afa0cdbfd1cd0057e532e
Daniel Tamura <danielhtelastico@gmail.com>
2016-10-26 08:20:37 -0200

    Adicionando porta e chave funcionais.

--- Pizzamancia/Assets/Scripts/Objetos/Objetos de Fase/Porta.cs
+++ Pizzamancia/Assets/Scripts/Objetos/Objetos de Fase/Porta.cs
@@ -1,9 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Porta : Objeto {
-	public void abrir () {
-		//this.AnimadorObjeto.SetTrigger ("abrir");
-		this.GetComponent<BoxCollider2D> ().enabled = false;
-	}
+public class Porta : Objeto
+{
+    #region atributos
+    bool isProntaParaAbrir;
+    #endregion
+
+    #region getters e setters
+    public bool IsProntaParaAbrir
+    {
+        get { return isProntaParaAbrir; }
+        set { isProntaParaAbrir = value; }
+    }
+    #endregion
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player" && isProntaParaAbrir)
+        {
+            this.AnimadorObjeto.SetTrigger("abrir");
+            this.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+
+    public void abrir()
+    {
+        this.AnimadorObjeto.SetTrigger("abrir");
+        this.GetComponent<BoxCollider2D>().enabled = false;
+    }
 }
