7e83ed1f07f410cd9509ff516e6022326a17da5d
Daniel Tamura <danielhtelastico@gmail.com>
2016-11-21 13:17:25 -0200

    Adicionando caixa de texto à HUD que aparece quando o jogador fica perto de um pergaminho que explica as coisas no tutorial (que também está sendo implementado nesse commit).

---
+++ Pizzamancia/Assets/Scripts/Interfaces In-Game/Caixa de Texto/CaixaTexto.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CaixaTexto : MonoBehaviour
+{
+    #region atributos
+    public static CaixaTexto caixaTextoInst;
+    Jogador jogador;
+    GameObject caixaDeTexto;
+    Text textoConteudo;
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+        caixaTextoInst = this;
+        jogador = GameObject.FindGameObjectWithTag("Player").GetComponent<Jogador>();
+        caixaDeTexto = GameObject.Find("Caixa de Texto");
+        textoConteudo = GameObject.Find("Texto Conteudo").GetComponent<Text>();
+
+        caixaDeTexto.SetActive(false);
+    }
+
+    public static CaixaTexto getInstance()
+    {
+        return caixaTextoInst;
+    }
+
+    public GameObject CaixaDeTexto
+    {
+        get { return caixaDeTexto; }
+        set { caixaDeTexto = value; }
+    }
+
+    public Text TextoConteudo
+    {
+        get { return textoConteudo; }
+        set { textoConteudo = value; }
+    }
+}
