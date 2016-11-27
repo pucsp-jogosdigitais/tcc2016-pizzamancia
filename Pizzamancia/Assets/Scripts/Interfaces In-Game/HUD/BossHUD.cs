2bef7a0690314c409d18643576d763255e5c634d
Daniel Haruo Tamura <danielhtelastico@gmail.com>
2016-11-27 20:57:41 -0200

    Adicionando script da HUD do boss.

---
+++ Pizzamancia/Assets/Scripts/Interfaces In-Game/HUD/BossHUD.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BossHUD : MonoBehaviour
+{
+	#region atributos
+	public static BossHUD bossHUDInst;
+	Boss bossRespectivo;
+	Text bossHUD;
+	Slider sliderBoss;
+	#endregion
+
+	// Use this for initialization
+	void Start()
+	{
+		bossHUDInst = this;
+		bossRespectivo = null;
+		bossHUD = GameObject.Find("Boss HUD").GetComponent<Text>();
+		sliderBoss = GameObject.Find("Slider Boss").GetComponent<Slider>();
+
+		sliderBoss.minValue = 0;
+
+		bossHUDInst.gameObject.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (bossRespectivo != null) 
+		{
+			bossHUD.text = bossRespectivo.VidaAtual + "/" + bossRespectivo.VidaTotal;
+			sliderBoss.value = bossRespectivo.VidaAtual;
+		} 
+		else 
+		{
+			bossHUD.text = "";
+			sliderBoss.value = 0;
+		}
+	}
+
+	public static BossHUD getInstance()
+	{
+		return bossHUDInst;
+	}
+
+	public void comecarLuta (Boss boss)
+	{
+		bossRespectivo = boss;
+
+		sliderBoss.maxValue = bossRespectivo.VidaTotal;
+
+		bossHUDInst.gameObject.SetActive (true);
+	}
+}
